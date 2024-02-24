b200b98eabafc6913ac32f1ae3e4277550ebba3f
Claudinei de Oliveira <claudinei@3cdesenvolvimento.com.br>
2024-02-24 04:46:02 -0400

    Tornando funcional a tela de Login, criando as rotas para cadastrar-se e recuperar a senha

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,4 +7,14 @@ use CodeIgniter\Router\RouteCollection;
  */
 // $routes->get('/', 'Home::index');
 
+/* Sign In / Login */
 $routes->get('/auth/login', 'Auth::login');
+$routes->post('/auth/login_submit', 'Auth::login_submit');
+
+/* Sign Up */
+$routes->get('/auth/signup', 'Auth::signup');
+$routes->post('/auth/signup_submit', 'Auth::signup_submit');
+
+/* Forgot Password */
+$routes->get('/auth/forgot_password', 'Auth::forgot_password');
+$routes->post('/auth/forgot_password_submit', 'Auth::forgot_password_submit');
--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -11,4 +11,29 @@ class Auth extends BaseController
     {
         return view('auth/login_frm');
     }
+
+    public function login_submit()
+    {
+        echo 'Login';
+    }
+
+    public function signup()
+    {
+        echo 'Sign Up';
+    }
+
+    public function signup_submit()
+    {
+        echo 'Sign Up Submit';
+    }
+
+    public function forgot_password()
+    {
+        echo 'Forgot Password';
+    }
+
+    public function forgot_password_submit()
+    {
+        echo 'Forgot Password Submit';
+    }
 }
